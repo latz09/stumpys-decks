1305ce1ee8139df1fc251503e421ca506fe83da2
latz09 <85132328+latz09@users.noreply.github.com>
2022-09-01 10:51:59 -0500

    form overlay

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -3,8 +3,8 @@ import FormInput from '../utils/FormInput';
 const ContactForm = () => {
 	return (
 		<form className='grid gap-6 place-items-center tracking-wider shadow-lg px-3 py-8  bg-white'>
-			<h1 className='text-3xl md:text-4xl text-orange mb-2'>{`Contact Stumpy's`}</h1>
-			<div className='grid grid-cols-2 gap-5 place-items-center px-2'>
+			<h1 className='text-3xl md:text-4xl text-center text-gray-700 mb-2'>Please fill out this form and we will get back to you.</h1>
+			<div className='grid lg:grid-cols-2 gap-5 place-items-center px-2'>
 				<FormInput type='text' placeholder='Name...' />
 				<FormInput type='email' placeholder='Email...' />
 				<FormInput type='tel' placeholder='Phone Number...' />
@@ -17,7 +17,7 @@ const ContactForm = () => {
 				<textarea className='w-full resize-none inputs' rows='8'></textarea>
 			</div>
 			<div>
-				<button className='btn px-4 py-2 text-2xl bg-orange text-white tracking-widest '>
+				<button className='btn px-4 py-2 text-2xl bg-blue text-white tracking-widest '>
 					Submit
 				</button>
 			</div>
--- pages/contact-us.js
+++ pages/contact-us.js
@@ -2,18 +2,13 @@ import ContactForm from '../components/contact/ContactForm';
 
 const Contact = () => {
 	return (
-		<div className='bg-gray-50 '>
-			<div className='max-w-7xl mx-auto grid py-12 px-1 '>
-				<div className='grid md:grid-cols-2 gap-4 place-items-center'>
+		<div className="bg-gray-50">
+			<div className='bg-blue text-white text-center  text-2xl md:text-4xl font-bold tracking-wider flex justify-center pt-12 pb-20'>
+				<p>We look forward to hearing from you!</p>
+			</div>
+			<div className='grid gap-4  p-4 md:w-1/2 mx-auto'>
+				<div className='relative bottom-16 bg-white shadow-lg'>
 					<ContactForm />
-					<div className="bg-yellow-400 p-2">
-						<div className='grid justify-items-center gap-3 text-3xl md:text-5xl tracking-wide  text-center bg-yellow-600 text-white p-12  shadow-lg '>
-							<h1 className=''>We Look Forward to hearing from you!</h1>
-							<span className='text-xl md:text-3xl'>
-								Please fill out this form and we will get back to you.
-							</span>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
@@ -21,3 +16,18 @@ const Contact = () => {
 };
 
 export default Contact;
+// <div className='bg-gray-50 '>
+// 	<div className='max-w-7xl mx-auto grid py-12 px-1 '>
+// 		<div className='grid md:grid-cols-2 gap-4 place-items-center'>
+// 			<ContactForm />
+// 			<div className="bg-yellow-400 p-2">
+// 				<div className='grid justify-items-center gap-3 text-3xl md:text-5xl tracking-wide  text-center bg-yellow-600 text-white p-12  shadow-lg '>
+// 					<h1 className=''>We Look Forward to hearing from you!</h1>
+// 					<span className='text-xl md:text-3xl'>
+// 						Please fill out this form and we will get back to you.
+// 					</span>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	</div>
+// </div>
