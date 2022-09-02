1e6ccfacc9bcb538ac5a019e2bc427dfb78ad90e
latz09 <85132328+latz09@users.noreply.github.com>
2022-09-02 13:03:11 -0500

    added links on construction page

--- components/utils/UnderConstruction.js
+++ components/utils/UnderConstruction.js
@@ -1,15 +1,36 @@
 import myLogo from '../../public/images/mylogo.png';
 import Image from 'next/image';
 import NewWindowLink from './NewWindowLink';
+import Link from 'next/link';
 const UnderConstruction = () => {
 	return (
 		<div className='bg-gray-100'>
-			<h1 className='text-center text-5xl py-12 tracking-widest  text-gray-700 '>
-				Just waiting on the go ahead!
+			<h1 className='text-center text-3xl md:text-5xl pt-12 tracking-widest  text-gray-700 '>
+				Just waiting on the go ahead for this page!
 			</h1>
+			<div className='max-w-4xl mx-auto grid place-items-center text-center py-12 mb-6 gap-4 '>
+				<h1 className='text-2xl md:text-4xl'>Other Pages on your redesign to explore--</h1>
+				<div className='flex space-x-4 items-center px-4'>
+					<Link href={'/'}>
+						<a className='p-2 md:p-3 bg-orange text-white md:text-xl font-bold tracking-widest'>
+							Home Page
+						</a>
+					</Link>
+					<Link href={'/deck-services'}>
+						<a className='p-2 md:p-3 bg-orange text-white md:text-xl font-bold tracking-widest'>
+							Deck Services
+						</a>
+					</Link>
+					<Link href={'/contact-us'}>
+						<a className='p-2 md:p-3 bg-orange text-white md:text-xl font-bold tracking-widest'>
+							Contact us
+						</a>
+					</Link>
+				</div>
+			</div>
 			<div className='grid md:grid-cols-3 max-w-5xl mx-auto place-items-center text-center'>
 				<div className='opacity-70 md:col-span-1 px-12 pb-3 md:p-0'>
-					<Image src={myLogo} alt='' layout='intrinsic' priority={true}/>
+					<Image src={myLogo} alt='' layout='intrinsic' priority={true} />
 				</div>
 				<div className='md:col-span-2 grid gap-4 text-left '>
 					<h1 className='text-xl md:text-3xl mx-8  md:leading-10'>
@@ -23,7 +44,6 @@ const UnderConstruction = () => {
 					<h1 className='text-xl md:text-3xl mx-8 md:leading-10 '>
 						I Look forward to working with you!
 					</h1>
-                  
 				</div>
 			</div>
 			<div className='grid gap-6 md:grid-cols-2 max-w-4xl mx-auto py-20 place-items-center '>
@@ -37,13 +57,16 @@ const UnderConstruction = () => {
 				</div>
 				<div className='text-2xl grid gap-1  border-2 p-3 border-[#40A9A1] bg-gray-50 shadow-lg'>
 					<div className='text-3xl font-bold'>Jordan Latz</div>
-					<div className="font-bold text-gray-600">Full Stack Web Develeoper</div>
+					<div className='font-bold text-gray-600'>
+						Full Stack Web Develeoper
+					</div>
 					<div className='text-lg'>
 						<div>715 630-5689</div>
-                        <div>jordan@latzwebdesign.com</div>
+						<div>jordan@latzwebdesign.com</div>
 					</div>
 				</div>
 			</div>
+	
 		</div>
 	);
 };
